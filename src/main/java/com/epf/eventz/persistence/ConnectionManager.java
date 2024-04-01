4792cdbc3ece07f02d6e57adc9840c7d52e1a433
NilsVincon <nils.vincon@epfedu.fr>
2024-04-01 17:43:52 +0200

    Ajout de ConnectionManager

--- src/main/java/com/epf/eventz/persistence/ConnectionManager.java
+++ src/main/java/com/epf/eventz/persistence/ConnectionManager.java
@@ -2,31 +2,22 @@ package com.epf.eventz.persistence;
 
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConnectionManager {
-    private static final String DB_CONNECTION = "jdbc:h2:~/EventzDatabase";
-    private static final String DB_USER = "";
+    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/projet_eventz";
+    private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "";
 
-    private static JdbcDataSource datasource = null;
 
-    private static void init() {
-        if (datasource == null) {
-            datasource = new JdbcDataSource();
-            datasource.setURL(DB_CONNECTION);
-            datasource.setUser(DB_USER);
-            datasource.setPassword(DB_PASSWORD);
-        }
-    }
 
     public static Connection getConnection() throws SQLException {
-        init();
-        return datasource.getConnection();
+
+        return DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
     }
 
 }
