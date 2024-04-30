10bfef73e743d526a91a66c012dab1cc8ec7982c
xavier <andreanixavier1@gmail.com>
2024-04-30 17:39:30 +0200

    ajout début homeadmin

---
+++ src/main/java/com/epf/eventz/exception/ControllerException.java
@@ -0,0 +1,7 @@
+package com.epf.eventz.exception;
+
+public class ControllerException extends Exception {
+    public ControllerException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/epf/eventz/servlet/HomeAdminController.java
@@ -0,0 +1,67 @@
+package com.epf.eventz.servlet;
+import com.epf.eventz.exception.ServiceException;
+import com.epf.eventz.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+@Controller
+public class HomeAdminController {
+    private final ArtisteService artisteService;
+    private final EvenementService evenementService;
+    private final UtilisateurService utilisateurService;
+    private final AdresseService adresseService;
+    private final TypeEvenementService typeEvenementService;
+    private final TypeMusiqueService typeMusiqueService;
+
+    @Autowired
+
+    public HomeAdminController(ArtisteService artisteService, EvenementService evenementService, UtilisateurService utilisateurService, AdresseService adresseService, TypeEvenementService typeEvenementService, TypeMusiqueService typeMusiqueService) {this.artisteService = artisteService;this.evenementService = evenementService;this.utilisateurService = utilisateurService;this.adresseService = adresseService;this.typeEvenementService = typeEvenementService;this.typeMusiqueService = typeMusiqueService;}
+
+    @GetMapping("/homeadmin")
+    public String gethome(Model model){
+        try {
+
+        }
+        catch (Exception e){
+        }
+        return("homeadmin");
+    }
+
+    private ArrayList<Objet> getObjets() {
+        ArrayList<Objet> objets = new ArrayList<>();
+        try {
+            objets.add(new Objet("Artiste", artisteService.compterArtistes()));
+            objets.add(new Objet("Evenement", evenementService.compterEvenements()));
+            objets.add(new Objet("Utilisateur", utilisateurService.compterUtilisateurs()));
+            objets.add(new Objet("Adresse", adresseService.compterAdresses()));
+            objets.add(new Objet("TypeEvenement", typeEvenementService.));
+            objets.add(new Objet("TypeMusique", evenementService.compterEvenements()));
+
+        } catch (ServiceException e) {
+            throw new RuntimeException(e);
+        }
+        return objets;
+    }
+    private static class Objet {
+        private String nom;
+        private int nbIterations;
+
+        public Objet(String nom, int nbIterations) {
+            this.nom = nom;
+            this.nbIterations = nbIterations;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public int getNbIterations() {
+            return nbIterations;
+        }
+    }
+}
