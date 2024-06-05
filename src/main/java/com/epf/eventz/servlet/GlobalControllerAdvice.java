e55bb6d1560f772f82fb3c2094c41d2e6d03136f
xavier <andreanixavier1@gmail.com>
2024-06-05 12:43:33 +0200

    petits trucs

--- src/main/java/com/epf/eventz/servlet/GlobalControllerAdvice.java
+++ src/main/java/com/epf/eventz/servlet/GlobalControllerAdvice.java
@@ -4,7 +4,6 @@ import com.epf.eventz.exception.ServiceException;
 import com.epf.eventz.model.Utilisateur;
 import com.epf.eventz.service.UtilisateurService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
